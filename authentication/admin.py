0db5ce8fc1982ec683c3b1e45b69279b411308a0
Tyler McAtee <tylermcatee@icloud.com>
2015-07-27 14:52:43 -0700

    Minor changes to photos

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,5 +3,7 @@ from models import *
 
 # Register your models here.
 class UserPhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display=('get_user_name',)
+    def get_user_name(self, obj):
+        return obj.user.username + "'s photo"
 admin.site.register(UserPhoto, UserPhotoAdmin)
